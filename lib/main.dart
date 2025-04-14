348c9aec345b061d5329956a9f9ed849b2eff812
ofix <shb8845369@gmail.com>
2025-04-14 17:37:15 +0800

    rename application name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RichApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RichApp extends StatelessWidget {
+  const RichApp({super.key});
 
   // This widget is the root of your application.
   @override
