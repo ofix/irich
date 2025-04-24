c527ea4e3da7b875af9118f822c9148b8d4bcc40
ofix <shb8845369@gmail.com>
2025-04-24 16:29:48 +0800

    update dependencies and development config files

--- lib/spider/stock_data_provider.dart
+++
@@ -1,8 +0,0 @@
-import "package:irich/store/stock.dart";
-
-abstract class StockDataProvider {
-  String get providerName;
-  Future<MinuteKline?> fetchMinuteKline(String shareCode); // 分时K线
-  Future<UiKline?> fetchDailyKline(String shareCode); // 日K线
-}
-
