2f15d4effb3f4b5111de4605c0b795c2c30aabee
ofix <shb8845369@gmail.com>
2025-05-08 16:17:52 +0800

    update global config path

--- lib/global/config.dart
+++ lib/global/config.dart
@@ -1,28 +1,29 @@
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 class Config {
   // 地域板块=>个股映射文件路径
   static Future<String> get pathMapFileProvince async {
     final appDir = await getApplicationDocumentsDirectory();
-    return "${appDir.path}/data/province.json";
+    return p.join(appDir.path, 'data', 'province.json');
   }
 
   // 行业板块=>个股映射文件路径
   static Future<String> get pathMapFileIndustry async {
     final appDir = await getApplicationDocumentsDirectory();
-    return "${appDir.path}/data/industry.json";
+    return p.join(appDir.path, 'data', 'industry.json');
   }
 
   // 概念板块=>个股映射文件路径
   static Future<String> get pathMapFileConcept async {
     final appDir = await getApplicationDocumentsDirectory();
-    return "${appDir.path}/data/concept.json";
+    return p.join(appDir.path, 'data', 'concept.json');
   }
 
   // 行情数据文件路径
   static Future<String> get pathDataFileQuote async {
     final appDir = await getApplicationDocumentsDirectory();
-    return "${appDir.path}/data/quote.json";
+    return p.join(appDir.path, 'data', 'quote.json');
   }
 
   // SQLite数据库文件
