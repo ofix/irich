cf92f9d9d950a4dafc3a4f2ef2d0a75a550bf6b6
ofix <shb8845369@gmail.com>
2025-05-08 22:27:10 +0800

    fix cross-platform compatibility issues with getApplicationDocumentRoot

--- lib/global/config.dart
+++ lib/global/config.dart
@@ -1,29 +1,51 @@
+import 'dart:io';
+
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
 class Config {
   // 地域板块=>个股映射文件路径
   static Future<String> get pathMapFileProvince async {
-    final appDir = await getApplicationDocumentsDirectory();
-    return p.join(appDir.path, 'data', 'province.json');
+    String appDir = "";
+    if (Platform.isWindows) {
+      appDir = Platform.resolvedExecutable;
+    } else {
+      appDir = (await getApplicationDocumentsDirectory()).path;
+    }
+    return p.join(appDir, 'data', 'province.json');
   }
 
   // 行业板块=>个股映射文件路径
   static Future<String> get pathMapFileIndustry async {
-    final appDir = await getApplicationDocumentsDirectory();
-    return p.join(appDir.path, 'data', 'industry.json');
+    String appDir = "";
+    if (Platform.isWindows) {
+      appDir = Platform.resolvedExecutable;
+    } else {
+      appDir = (await getApplicationDocumentsDirectory()).path;
+    }
+    return p.join(appDir, 'data', 'industry.json');
   }
 
   // 概念板块=>个股映射文件路径
   static Future<String> get pathMapFileConcept async {
-    final appDir = await getApplicationDocumentsDirectory();
-    return p.join(appDir.path, 'data', 'concept.json');
+    String appDir = "";
+    if (Platform.isWindows) {
+      appDir = Platform.resolvedExecutable;
+    } else {
+      appDir = (await getApplicationDocumentsDirectory()).path;
+    }
+    return p.join(appDir, 'data', 'concept.json');
   }
 
   // 行情数据文件路径
   static Future<String> get pathDataFileQuote async {
-    final appDir = await getApplicationDocumentsDirectory();
-    return p.join(appDir.path, 'data', 'quote.json');
+    String appDir = "";
+    if (Platform.isWindows) {
+      appDir = Platform.resolvedExecutable;
+    } else {
+      appDir = (await getApplicationDocumentsDirectory()).path;
+    }
+    return p.join(appDir, 'data', 'quote.json');
   }
 
   // SQLite数据库文件
