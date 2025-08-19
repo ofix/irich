d3ed4fcfc37799d037b113321814e41ea0529870
ofix <shb8845369@gmail.com>
2025-08-19 18:16:43 +0800

    update sina quote provider

--- lib/service/api_providers/api_provider_sina.dart
+++ lib/service/api_providers/api_provider_sina.dart
@@ -10,6 +10,7 @@
 // ignore_for_file: avoid_print
 import "dart:convert";
 
+import "package:flutter/cupertino.dart";
 import "package:irich/service/api_provider_capabilities.dart";
 import "package:irich/service/api_providers/api_provider.dart";
 import "package:irich/global/stock.dart";
@@ -19,7 +20,7 @@ import "package:irich/service/request_log.dart";
 // 和讯网股票列表获取函数
 String shareListUrlSina(String market, int pageOffset, int pageSize) {
   return "https://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData"
-      "?page=1"
+      "?page=$pageOffset"
       "&num=$pageSize"
       "&sort=changepercent"
       "&asc=0"
@@ -62,43 +63,73 @@ class ApiProviderSina extends ApiProvider {
   // 获取股票列表
   Future<dynamic> fetchQuote() async {
     try {
-      final List<int> markets = [1, 1, 1, 2, 2, 2, 6, 6, 1789];
+      final markets = ['sh_a', 'sz_a']; // 沪市A股和深市A股
+      final counts = await Future.wait([
+        fetchMarketShareCount(markets[0]), // 沪市A股
+        fetchMarketShareCount(markets[1]), // 深市A股(主板+创业板)
+      ]);
+      for (var i = 0; i < markets.length; i++) {
+        debugPrint('market ${markets[i]} share count: ${counts[i]}');
+      }
       final results = await Future.wait([
-        _fetchMarketShares('sh_a', 1, 3000), // 沪市A股
-        _fetchMarketShares('sz_a', 1, 3000), // 深市A股
-        _fetchMarketShares('', 0, 1000), // 创业板
+        _fetchMarketShares('sh_a', counts[0]), // 沪市A股
+        _fetchMarketShares('sz_a', counts[1]), // 深市A股
       ]);
       for (var i = 0; i < results.length; i++) {
-        marketShares.addAll(_parseMarketShare(results[i], markets[i]));
+        marketShares.addAll(results[i]);
       }
-      // 股票去重
-      marketShares = removeDuplicates(marketShares);
       return marketShares;
     } catch (e) {
       throw Exception('Failed to fetch market shares: $e');
     }
   }
 
+  // 获取交易所股票个数
+  Future<int> fetchMarketShareCount(String market) async {
+    try {
+      final url =
+          "https://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeStockCount?node=$market";
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final data = response.body;
+        String count = data.trim().replaceAll(RegExp(r'[^0-9]'), '');
+        final shareCount = int.parse(count);
+        return shareCount;
+      } else {
+        throw Exception('Failed to request market shares count, $url');
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch market share count: $e');
+    }
+  }
+
   // 获取和讯网股票列表数据
-  Future<dynamic> _fetchMarketShares(String market, int pageOffset, int pageSize) async {
-    final url = shareListUrlSina(market, pageOffset, pageSize);
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final data = response.body.replaceAll(RegExp(r'^\(|\);$'), '');
-      return data;
-    } else {
-      throw Exception('Failed to request market shares, $url');
+  Future<List<Share>> _fetchMarketShares(String market, int count) async {
+    final List<Share> shares = [];
+    int pageOffset = 1;
+    int pageSize = 100; // 每页大小
+    try {
+      while (pageOffset * pageSize <= count) {
+        final url = shareListUrlSina(market, pageOffset, pageSize);
+        debugPrint("[行情][新浪]: $url");
+        final response = await http.get(Uri.parse(url));
+        if (response.statusCode == 200) {
+          final sharesInPage = _parseMarketShare(response.body, market);
+          shares.addAll(sharesInPage);
+        }
+        if (shares.length == count) break;
+        pageOffset++;
+      }
+      return shares;
+    } catch (e) {
+      debugPrint("分页获取新浪财经股票列表失败: $e");
+      rethrow;
     }
   }
 
   // 将市场类型转换为 Market 枚举
-  Market _toMarket(int market) {
-    final kv = {
-      1: Market.shangHai,
-      2: Market.shenZhen,
-      6: Market.chuangYeBan,
-      1789: Market.keChuangBan,
-    };
+  Market _toMarket(String market) {
+    final kv = {"sh_a": Market.shangHai, "sz_a": Market.shenZhen};
     if (kv.containsKey(market)) {
       return kv[market]!;
     } else {
@@ -107,37 +138,35 @@ class ApiProviderSina extends ApiProvider {
   }
 
   // 解析股票列表返回结果
-  List<Share> _parseMarketShare(String response, int market) {
+  List<Share> _parseMarketShare(String response, String market) {
     List<Share> shares = [];
+    String code = "";
     try {
-      final jsonData = jsonDecode(response);
-      // final count = jsonData['Total'] as int;
-      final arr = jsonData['Data'][0] as List;
-
-      for (final item in arr) {
-        final factor = item[9];
+      final arr = jsonDecode(response) as List<dynamic>;
+      for (final json in arr) {
+        final item = json as Map<String, dynamic>;
         final share = Share(
-          code: item[0].toString(),
-          name: (item[1] as String).replaceAll(' ', ''),
+          code: item['code'] as String,
+          name: item['name'] as String,
           market: _toMarket(market),
-          priceYesterdayClose: item[4] / factor,
-          priceNow: item[2] / factor,
-          changeRate: item[3] / 10000,
-          priceOpen: item[5] / factor,
-          priceMax: item[6] / factor,
-          priceMin: item[7] / factor,
-          volume: (item[8] / 100).toInt(),
-          amount: ((item[10] as num).toDouble()),
-          turnoverRate: item[11] / 10000,
-          priceAmplitude: item[12] / 10000,
-          qrr: item[13] / 100,
+          priceYesterdayClose: double.tryParse((item['settlement'] ?? "")) ?? 0.0,
+          priceNow: double.tryParse((item['buy'] ?? "")) ?? 0.0,
+          changeRate: (item['changepercent'] as num).toDouble(),
+          priceOpen: double.tryParse((item['open'] ?? "")) ?? 0.0,
+          priceMax: double.tryParse((item['high'] ?? "")) ?? 0.0,
+          priceMin: double.tryParse((item['low'] ?? "")) ?? 0.0,
+          volume: item['volume'] as int,
+          amount: (item['amount'] as num).toDouble(),
+          turnoverRate: (item['turnoverratio'] as num).toDouble(),
+          priceAmplitude: (item['pricechange'] as num).toDouble(),
+          qrr: 0.0,
         );
-
+        code = item['code'] as String;
         shares.add(share);
       }
       return shares;
     } catch (e) {
-      throw Exception('Failed to parse stock data: $e');
+      throw Exception('Failed to parse stock data: $e, code : $code,response: $response');
     }
   }
 }
